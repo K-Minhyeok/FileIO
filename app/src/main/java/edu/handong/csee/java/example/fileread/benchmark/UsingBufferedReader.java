a1bb82e04bd27c27ad6f6192e3afdd73d8ca3631
K-Minhyeok <gurdl2384@naver.com>
2023-05-18 15:19:39 +0900

    "modified UsingBufferedReader.java"

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -12,27 +12,43 @@ public class UsingBufferedReader {
         //-------------- Test reading 1 MB file. --------------------
 
         StopWatch.start();
-
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+       
+        if(args[0].endsWith(".txt")) {
+        	System.out.print(args[0]+"파일을 읽습니다.\n");
+        
+        
+        BufferedReader inputStream= new BufferedReader(new FileReader(args[0]));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
         System.out.println(duration + " milsec");
         
         inputStream.close();
-
+        }
+        
+        else {
+        	System.out.print("파일이 존재하지 않습니다.\n");
+        	System.exit(0);
+        }
 
         //-------------- Test reading 10 MB file. --------------------
 
         StopWatch.start();
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        if(args[1].endsWith(".txt")) {
+        	System.out.print(args[1]+"파일을 읽습니다.\n");
+        	
+        BufferedReader inputStream2= new BufferedReader(new FileReader(args[1]));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
         System.out.println(duration2 + " milsec");
 
         inputStream2.close();
+        }      else {
+        	System.out.print("파일이 존재하지 않습니다.\n");
+        	System.exit(0);
+        }
 
         /*
         //-------------- Test reading 100 MB file. --------------------
